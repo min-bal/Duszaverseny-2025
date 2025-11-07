0a34e57b6bb9553dc0363218fd4ce38fd3efb546
toPyNtet <peter.rozsa09@gmail.com>
2025-11-07 13:28:45 +0100

    buzi

--- Duszaverseny-2025/Form1.cs
+++ Duszaverseny-2025/Form1.cs
@@ -7,15 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Duszaverseny_2025
 {
     public partial class Form1 : Form
     {
+        string[] nevek;
+        string[] sebzes;
+        string[] elet;
+        string[] tipus;
+
+        var kartyak = new Dictionary<string, int, int, string>
+        {
+
+        }
         public Form1()
         {
+            string[] args = Environment.GetCommandLineArgs();
+            StreamReader sr = new StreamReader(args[1]);
             InitializeComponent();
-            label1.Text = "Hello World!";
+        }
+
+        //in.txt beolvasás
+        private void beolvasás(StreamReader sr)
+        {
+            int n = 0;
+            string sor = sr.ReadLine();
+            string[] sorreszek = sor.Split(';');
+            if (sorreszek[0] == "uj kartya")
+            {
+                nevek[n] = sorreszek[1];
+                sebzes[n] = sorreszek[2];
+                elet[n] = sorreszek[3];
+                tipus[n] = sorreszek[4];
+                n++;
+            }
+            else if (sorreszek[1] == "uj vezer")
+            {
+                nevek[n] = sorreszek[1];
+                if (sorreszek[3] == "sebzes")
+                {
+                    string vezer = sorreszek[2];
+                    bool megvan = false;
+                    int m = 0;
+                    while (megvan = false)
+                    {
+                        if (nevek[m])
+                    }
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
