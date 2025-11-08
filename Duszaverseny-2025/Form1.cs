0e0ea41b1e124989fe86ab42f66ce2f72c958554
toPyNtet <peter.rozsa09@gmail.com>
2025-11-08 11:58:36 +0100

    kartyak, dungeonok

--- Duszaverseny-2025/Form1.cs
+++ Duszaverseny-2025/Form1.cs
@@ -26,6 +26,52 @@ namespace Duszaverseny_2025
 
     public partial class Form1 : Form
     {
+        Dictionary<int, (string, int, int, string)> kartyak = new Dictionary<int, (string, int, int, string)> //név, sebzés, életerő, típus
+        {
+            {0, ("Arin", 2, 5, "Föld") },
+            {1, ("Liora", 2, 4, "Levegő") },
+            {2, ("Nerun", 3, 3, "Tűz") },
+            {3, ("Selia", 2, 6, "Víz") },
+            {4, ("Torak", 3, 4, "Föld") },
+            {5, ("Emera", 2, 5, "Levegő") },
+            {6, ("Vorn", 2, 7, "Víz") },
+            {7, ("Kael", 3, 5, "Tűz") },
+            {8, ("Myra", 2, 6, "Föld") },
+            {9, ("Thalen", 3, 5, "Levegő") },
+            {10, ("Isara", 2, 6, "Víz") }        
+        }; 
+        Dictionary<int, (string, int, int, string, string)> vezerkartyak = new Dictionary<int, (string, int, int, string, string)>
+        {
+            {0, ("Lord Torak", 6, 4, "Föld", "(Vezér)") },
+            {1, ("Priestess Selia", 2, 12, "Föld", "(Vezér)") }
+        };
+        Dictionary<int, (string, int, int, string)> playercards = new Dictionary<int, (string, int, int, string)> //properies of the player's cards
+        {
+            {0, ("Arin", 2, 5, "Föld") },
+            {1, ("Liora", 2, 4, "Levegő") },
+            {2, ("Selia", 2, 6, "Víz") },
+            {3, ("Nerun", 3, 3, "Tűz") },
+            {4, ("Torak", 3, 4, "Föld") },
+            {5, ("Emera", 2, 5, "Levegő") },
+            {6, ("Kael", 3, 5, "Tűz") },
+            {7, ("Myra", 2, 6, "Föld") },
+            {8, ("Thalen", 3, 5, "Levegő") },
+            {9, ("Isara", 2, 6, "Víz") }
+        };
+
+        Dictionary<string, (string, string)> kazamataegyszeru = new Dictionary<string, (string, string)> //név, ellenfél, jutalom
+        {
+            {"Barlangi Portya", ("Nerun", "sebzés") }
+        };
+        Dictionary<string, (string, string, string, string, string)> kazamatakicsi = new Dictionary<string, (string, string, string, string, string)> //név, ellenfél*3, vezér, jutalom
+        {
+            {"Osi Szentely", ("Arin", "Emera", "Selia", "Lord Torak", "életerő") }
+        }; 
+        Dictionary<string, (string, string, string, string, string, string)> kazamatanagy = new Dictionary<string, (string, string, string, string, string, string)> //név, ellenfél*5, vezér
+        {
+            {"A melyseg kiralynoje", ("Liora", "Arin", "Selia", "Nerun", "Torak", "Priestess Selia") }
+        };
+
         public Form1()
         {
             InitializeComponent();
